c53edfeb4700fe254952bc187a6a6b27485249bc
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-08-30 22:33:25 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,13 +12,13 @@ export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
       )}
       {...props}
     >
-      Open source AI chatbot built with{' '}
+      Built by Priyansh using Next.js{' '}
       <ExternalLink href="https://cloud.google.com/vertex-ai">
-        Google Gemini
+        Google Gemini API
       </ExternalLink>
       , <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
       <ExternalLink href="https://github.com/vercel/ai">
-        Vercel AI SDK
+        Google AI SDK
       </ExternalLink>
       .
     </p>
