b30b11d7342e731eddf69797e4e19a3734f4ef3d
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-09-01 09:50:48 +0530

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -5,17 +5,17 @@ export function EmptyScreen() {
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:p-8 p-4 text-sm sm:text-base">
         <h1 className="text-2xl sm:text-3xl tracking-tight font-semibold max-w-fit inline-block">
-         Solve your plant related doubts
+          PlantDoc Chatbot
         </h1>
         <p className="leading-normal text-zinc-900">
-          AI chatbot app{' '}
+          This is an open source AI chatbot app {' '}
           <ExternalLink href="https://nextjs.org">Next.js</ExternalLink>, the{' '}
           <ExternalLink href="https://sdk.vercel.ai">
-          
+           Plantdoc
           </ExternalLink>
           , and{' '}
           <ExternalLink href="https://ai.google.dev">
-            Google Gemini
+            Plantdoc AI
           </ExternalLink>
           .
         </p>
@@ -24,7 +24,7 @@ export function EmptyScreen() {
           <ExternalLink href="https://vercel.com/blog/ai-sdk-3-generative-ui">
             React Server Components
           </ExternalLink>{' '}
-          
+
         </p>
       </div>
     </div>
