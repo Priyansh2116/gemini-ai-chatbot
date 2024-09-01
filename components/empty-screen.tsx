27cea3b9a67960bec7290939819c3d1c851c2f68
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-09-01 09:12:45 +0530

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -5,13 +5,13 @@ export function EmptyScreen() {
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:p-8 p-4 text-sm sm:text-base">
         <h1 className="text-2xl sm:text-3xl tracking-tight font-semibold max-w-fit inline-block">
-          Ask about your plant's diseases
+         Solve your plant related doubts
         </h1>
         <p className="leading-normal text-zinc-900">
-          This chatbot is built to answer your plant questions{' '}
+          This is an open source AI chatbot app template built with{' '}
           <ExternalLink href="https://nextjs.org">Next.js</ExternalLink>, the{' '}
           <ExternalLink href="https://sdk.vercel.ai">
-            Plantdoc
+            Vercel AI SDK
           </ExternalLink>
           , and{' '}
           <ExternalLink href="https://ai.google.dev">
@@ -22,9 +22,12 @@ export function EmptyScreen() {
         <p className="leading-normal text-zinc-900">
           It uses{' '}
           <ExternalLink href="https://vercel.com/blog/ai-sdk-3-generative-ui">
-            React Server Components.
+            React Server Components
           </ExternalLink>{' '}
-         Solve all your plant disease doubts 
+          with function calling to mix both text with generative UI responses
+          from Gemini. The UI state is synced through the AI SDK so the model is
+          always aware of your stateful interactions as they happen in the
+          browser.
         </p>
       </div>
     </div>
